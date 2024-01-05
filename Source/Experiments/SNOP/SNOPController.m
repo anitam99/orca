68bd1753bc205cea3b24f0224d17f7e3979f246f
ac_masuskapoe <ac_masuskapoe@ac-masuskapoes-MacBook-Pro.local>
2024-01-05 14:07:27 -0500

    Adding number formatting to CommandCenter.nib port number text box

--- Source/Experiments/SNOP/SNOPController.m
+++ Source/Experiments/SNOP/SNOPController.m
@@ -829,7 +829,7 @@ snopGreenColor;
      * stop run and then start run. Doing this resets the GTID, which resyncs
      * crate 9 after it goes out of sync :). */
 
-    NSLog(@"Inside the Resync Run Action");
+    NSLog(@"Inside the Resync Action");
     
     [model setResync:YES];
 
@@ -842,7 +842,6 @@ snopGreenColor;
     [self endEditing];
     [model stopRun];
 }
-
 
 - (void) runStatusChanged:(NSNotification*)aNotification
 {
